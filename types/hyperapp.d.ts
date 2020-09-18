1b1a837479e7ffc12e445e7534e01717a17878aa
Ron Martinez <ramartin.net@gmail.com>
2020-09-18 01:34:43 -0400

    wip

--- types/hyperapp.d.ts
+++ types/hyperapp.d.ts
@@ -1,123 +1,8 @@
 // Minimum TypeScript Version: 3.7
 
 declare module "hyperapp" {
-  // A Hyperapp application instance has an initial state and a base view.
-  // It must also be mounted over an available DOM element.
-  type App<S, P = unknown, D = unknown>
-    = Readonly<{
-      init: Transition<S, P, D> | Action<S, P, D>
-      view: View<S, D>
-      node: Node
-      subscriptions?: Subscription<S, P, D>
-      middleware?: Middleware<S, P, D>
-    }>
-
-  // A transition is a state transformation with any effects to run.
-  type Transition<S, P = unknown, D = unknown> = State<S> | StateWithEffects<S, P, D>
-
-  // Application state is accessible in every view, action, and subscription.
-  type State<S> = S
-
-  // Transformed state can be paired with a list of effects to run.
-  type StateWithEffects<S, P = unknown, D = unknown> = [State<S>, ...EffectDescriptor<S, P, D>[]]
-
-  // A view builds a virtual DOM node representation of the application state.
-  type View<S, D = unknown> = (state: State<S>) => VDOM<S, D>
-
-  // A subscription is a set of recurring effects.
-  type Subscription<S, P = unknown, D = unknown> = (state: State<S>) => Subscriber<S, P, D>[]
-
-  // A subscriber reacts to subscription updates.
-  type Subscriber<S, P = unknown, D = unknown> = boolean | undefined | Effect<S, P, D> | Unsubscribe
-
-  // A subscriber ideally provides a function that cancels itself properly.
-  type Unsubscribe = () => void
-
-  // Middleware allows for custom processing during dispatching.
-  type Middleware<S, P = unknown, D = unknown> = (dispatch: Dispatch<S, P, D>) => Dispatch<S, P, D>
-
-  // ---------------------------------------------------------------------------
-
-  // A dispatched action handles an event in the context of the current state.
-  type Dispatch<S, P = unknown, D = unknown> = (action: Action<S, P, D>, props?: Payload<P>) => void
-
-  // An action transforms existing state and can be wrapped by another action.
-  type Action<S, P = unknown, D = unknown>
-    = [Action<S, P, D>, Payload<P>]
-    | ((state: State<S>, props?: Payload<P>) => Transition<S, P, D> | Action<S, P, D>)
-
-  // A payload is data external to state that is given to a dispatched action.
-  type Payload<P = unknown> = P
-
-  // An effect descriptor describes how an effect should be invoked.
-  // A function that creates this is called an effect constructor.
-  type EffectDescriptor<S, P = unknown, D = unknown> = [Effect<S, P, D>, EffectData<D>]
-
-  // An effect is where side effects and any additional dispatching occur.
-  // An effect used in a subscription should be able to unsubscribe.
-  type Effect<S, P = unknown, D = unknown>
-    = (dispatch: Dispatch<S, P, D>, props?: EffectData<D>) =>
-        void | Unsubscribe | Promise<undefined | Unsubscribe>
-
-  // An effect is generally given additional data.
-  type EffectData<D = unknown> = D
-
-  // ---------------------------------------------------------------------------
-
-  // A virtual DOM node represents an actual DOM element.
-  type VDOM<S, D = unknown> = {
-    readonly type: string
-    readonly props: PropList<S, D>
-    readonly children: VNode<S, D>[]
-    node: MaybeNode
-    readonly tag?: Tag<S, D>
-    readonly key: Key
-    memo?: PropList<S, D>
-  }
-
-  // Virtual DOM properties will often correspond to HTML attributes.
-  type PropList<S, D = unknown>
-    = Readonly<ElementCreationOptions & EventActions<S, D> & {
-      [_: string]: unknown
-      class?: ClassProp
-      key?: Key
-      style?: StyleProp
-    }>
-
-  // Actions are used as event handlers.
-  type EventActions<S, D = unknown> = { [K in keyof EventsMap]?: Action<S, EventsMap[K], D> }
-  type EventsMap = OnHTMLElementEventMap & OnWindowEventMap & { onsearch: Event }
-
-  // A key can uniquely associate a virtual DOM node with a certain DOM element.
-  type Key = string | null | undefined
-
-  // The `class` property represents an HTML class attribute string.
-  type ClassProp = false | string | Record<string, boolean> | ClassProp[]
-
-  // The `style` property represents inline CSS.
-  type StyleProp
-    = { [K in keyof CSSStyleDeclaration]?: CSSStyleDeclaration[K] | null }
-    // For some reason we need this to prevent `style` from being a string.
-    & { [index: number]: never }
-
-  // A virtual node is a convenience layer over a virtual DOM node.
-  type VNode<S, D = unknown> = false | null | undefined | VDOM<S, D>
-
-  // Actual DOM nodes will be manipulated depending on how property patching goes.
-  type MaybeNode = null | undefined | Node
-
-  // A virtual DOM node's tag has metadata relevant to it. Virtual DOM nodes are
-  // tagged by their type to assist rendering.
-  type Tag<S, D = unknown> = VDOMNodeType | View<S, D>
-
-  // These are based on actual DOM node types:
-  // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
-  const enum VDOMNodeType { SSR = 1, Text = 3 }
-
-  // ---------------------------------------------------------------------------
-
-  // The `app` function initiates a Hyperapp application. `app` along with effects
-  // should be the only places you need to worry about side effects.
+  // The `app` function initiates a Hyperapp application. `app` along with
+  // effects are the only places where side effects are allowed.
   function app<S, P = unknown, D = unknown>(props: App<S, P, D>): Dispatch<S, P, D>
 
   // The `h` function builds a virtual DOM node.
@@ -132,6 +17,121 @@ declare module "hyperapp" {
 
   // The `text` function creates a virtual DOM node representing plain text.
   function text<S, D = unknown>(value: number | string, node?: Node): VDOM<S, D>
+
+  // ---------------------------------------------------------------------------
+
+  // A Hyperapp application instance has an initial state and a base view.
+  // It must also be mounted over an available DOM element.
+  type App<S, P = unknown, D = unknown>
+    = Readonly<{
+      init: Transition<S, D> | Action<S, P, D>
+      view: View<S, D>
+      node: Node
+      subscriptions?: Subscription<S, D>
+      middleware?: Middleware<S, P, D>
+    }>
+
+  // A view builds a virtual DOM node representation of the application state.
+  type View<S, D = unknown> = (state: State<S>) => VDOM<S, D>
+
+  // A subscription is a set of recurring effects.
+  type Subscription<S, D = unknown> = (state: State<S>) => Subscriber<S, D>[]
+
+  // A subscriber reacts to subscription updates.
+  type Subscriber<S, D = unknown> = boolean | undefined | Effect<S, D> | Unsubscribe
+
+  // A subscriber ideally provides a function that cancels itself properly.
+  type Unsubscribe = () => void
+
+  // Middleware allows for custom processing during dispatching.
+  type Middleware<S, P = unknown, D = unknown> = (dispatch: Dispatch<S, P, D>) => Dispatch<S, P, D>
+
+  // ---------------------------------------------------------------------------
+
+  // A dispatched action handles an event in the context of the current state.
+  type Dispatch<S, P = unknown, D = unknown> = (action: Action<S, P, D>, props?: Payload<P>) => void
+
+  // An action transforms existing state and/or wraps another action.
+  type Action<S, P = unknown, D = unknown>
+    = ((state: State<S>, props?: Payload<P>) => Transition<S, D> | Action<S>)
+    | ActionDescriptor<S, P, D>
+
+  // An action descriptor describes an action and any payload for it.
+  type ActionDescriptor<S, P, D> = [Action<S, P, D>, Payload<P>]
+
+  // A payload is data external to state that is given to an action or effect.
+  type Payload<P = unknown> = P
+
+  // A transition is a state transformation with any effects to run.
+  type Transition<S, D = unknown> = State<S> | StateWithEffects<S, D>
+
+  // Application state is accessible in every view, action, and subscription.
+  type State<S> = S
+
+  // Transformed state can be paired with a list of effects to run.
+  type StateWithEffects<S, D = unknown> = [State<S>, ...EffectDescriptor<S, D>[]]
+
+  // An effect descriptor describes how an effect should be invoked.
+  // A function that creates this is called an effect constructor.
+  type EffectDescriptor<S, D = unknown> = [Effect<S, D>, Payload<D>]
+
+  // An effect is where side effects and any additional dispatching occur.
+  // An effect used in a subscription should be able to unsubscribe.
+  type Effect<S, D = unknown>
+    = (dispatch: Dispatch<S, unknown, D>, props?: Payload<D>) =>
+        void | Unsubscribe | Promise<undefined | Unsubscribe>
+
+  // ---------------------------------------------------------------------------
+
+  // A virtual DOM node represents an actual DOM element.
+  type VDOM<S, D = unknown> = {
+    readonly type: string
+    readonly props: PropList<S, D>
+    readonly children: VNode<S, D>[]
+    node: MaybeNode
+    readonly tag?: Tag<S, D>
+    readonly key: Key
+    memo?: PropList<S, D>
+  }
+
+  // A key can uniquely associate a virtual DOM node with a certain DOM element.
+  type Key = string | null | undefined
+
+  // Actual DOM nodes will be manipulated depending on how property patching goes.
+  type MaybeNode = null | undefined | Node
+
+  // A virtual node is a convenience layer over a virtual DOM node.
+  type VNode<S, D = unknown> = false | null | undefined | VDOM<S, D>
+
+  // A virtual DOM node's tag has metadata relevant to it. Virtual DOM nodes are
+  // tagged by their type to assist rendering.
+  type Tag<S, D = unknown> = VDOMNodeType | View<S, D>
+
+  // These are based on actual DOM node types:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+  const enum VDOMNodeType { SSR = 1, Text = 3 }
+
+  // Virtual DOM properties will often correspond to HTML attributes.
+  type PropList<S, D = unknown>
+    = Readonly<ElementCreationOptions & EventActions<S, D> & {
+      [_: string]: unknown
+      class?: ClassProp
+      key?: Key
+      style?: StyleProp
+    }>
+
+  // The `class` property represents an HTML class attribute string.
+  type ClassProp = false | string | Record<string, boolean> | ClassProp[]
+
+  // The `style` property represents inline CSS.
+  type StyleProp
+    = { [K in keyof CSSStyleDeclaration]?: CSSStyleDeclaration[K] | null }
+    // For some reason we need this to prevent `style` from being a string.
+    & { [index: number]: never }
+
+  // Event handlers are implemented using actions.
+  type EventActions<S, D = unknown> = { [K in keyof EventsMap]?: Action<S, EventsMap[K], D> }
+  type EventsMap = OnHTMLElementEventMap & OnWindowEventMap & { onsearch: Event }
 
   // ---------------------------------------------------------------------------
 
