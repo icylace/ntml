6e4af07ece88f2fd1fbc1f284966a1552023221e
Ron Martinez <ramartin.net@gmail.com>
2021-04-20 15:56:04 -0400

    wip

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,9 +8,9 @@ const myN = typedN<State>()
 
 const a = myN("a")
 const x0 = a("hi")
-const x1 = a<State>("hi")
+// const x1 = a<State>("hi")    // Intentionally errors.
 const x2: VDOM<State> = a("hi")
-const x3: VDOM<State> = a<State>("hi")
+// const x3: VDOM<State> = a<State>("hi")    // Intentionally errors.
 
 const b = n("b")
 const x4 = b("hjgkjhg")
